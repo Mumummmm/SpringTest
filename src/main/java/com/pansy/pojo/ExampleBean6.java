bc84a680f88a6d52d58269300431a83a223a5ec3
pansy <526915701@qq.com>
2017-12-05 14:10:05 +0800

    3.4.5 自动装配

---
+++ src/main/java/com/pansy/pojo/ExampleBean6.java
@@ -0,0 +1,9 @@
+package com.pansy.pojo;
+
+public class ExampleBean6 {
+    private ManagerBean manager;
+
+    public void setManager(ManagerBean manager) {
+        this.manager = manager;
+    }
+}
